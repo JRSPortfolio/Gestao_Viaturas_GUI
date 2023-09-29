b789262f065c808cfa079202f9ce26e0a4d39bb9
JRSPortfolio <jrsalgueiro@live.com.pt>
2023-09-29 15:40:58 +0100

    addded listar option

--- viaturas_gui.py
+++ viaturas_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font, ttk, Scrollbar
+from tkinter.scrolledtext import ScrolledText
 import winreg
 from op_ficheiros import *
 
@@ -11,24 +12,21 @@ class janela_inicial(tk.Tk):
         self.title("Catalogo de Automóveis")
         self.geometry("600x800+100+50")
         
-        self.rowconfigure(0, weight = 0)
-        self.rowconfigure(1, weight = 0)
-        self.rowconfigure(2, weight = 0)
-        self.rowconfigure(3, weight = 0)
-        self.columnconfigure(0, weight = 1)
-        self.columnconfigure(1, weight = 1)
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)
         
         # fonts = font.families()
         title_font = ("Cascadia Mono", 16, "bold")
         button_font = ("Cascadia Mono", 12, "bold")
-        result_titles_font = ("Cascadia Mono", 12, "bold")
+        self.result_titles_font = ("Cascadia Mono", 12, "bold")
+        self.detail_labels = []
         
         self.title_label = tk.Label(self, text = "Catálogo de Automóveis", font = title_font)
         self.title_label.grid(row = 0, column = 0, columnspan = 2, padx = 40, pady = 30, sticky = "ew")
         
         self.buttons_layout(button_font)
-        self.result_layout(result_titles_font)
-        self.limpar_result_layout(result_titles_font)
+        self.result_layout(self.result_titles_font)
+        self.limpar_result_layout(self.result_titles_font)
         self.menu()
           
     def menu(self):
@@ -94,61 +92,98 @@ class janela_inicial(tk.Tk):
         self.lista_fontes_canvas.bind_all("<MouseWheel>", on_mousewheel)
                     
     def buttons_layout(self, font): 
-        self.listar_viaturas = tk.Button(self, text="Listar Viaturas", width = 12, height = 2 ,
+        self.listar_viaturas = tk.Button(self, text="Listar Viaturas", width = 30, height = 2 ,
                                          font = font, command = self.action_listar_viaturas)
-        self.pes_viaturas = tk.Button(self, text="Pesquisar Viaturas", width = 12, height = 2,
+        self.pes_viaturas = tk.Button(self, text="Pesquisar Viaturas", width = 30, height = 2,
                                       font = font, command=lambda: print("click"))
-        self.add_viatura = tk.Button(self, text="Adicionar Viatura",  width = 12, height = 2,
+        self.add_viatura = tk.Button(self, text="Adicionar Viatura",  width = 30, height = 2,
                                      font = font, command=lambda: print("click"))
-        self.rem_viatura = tk.Button(self, text="Remover Viatura",  width = 12, height = 2,
+        self.rem_viatura = tk.Button(self, text="Remover Viatura",  width = 30, height = 2,
                                      font = font, command=lambda: print("click"))
-        self.gravar_catalogo = tk.Button(self, text="Gravar Catalogo",  width = 12, height = 2,
+        self.gravar_catalogo = tk.Button(self, text="Gravar Catalogo",  width = 30, height = 2,
                                          font = font, command=lambda: print("click"))
-        self.recarregar_catalogo = tk.Button(self, text="Recarregar Catalogo",  width = 12, height = 2,
+        self.recarregar_catalogo = tk.Button(self, text="Recarregar Catalogo",  width = 30, height = 2,
                                              font = font, command=lambda: print("click"))
         
-        self.listar_viaturas.grid(row = 1, column = 0, padx = 40, pady = 10, sticky = "ew")
-        self.pes_viaturas.grid(row = 1, column = 1, padx = 40, pady = 10, sticky = "ew")
-        self.add_viatura.grid(row = 2, column = 0, padx = 40, pady = 10, sticky = "ew")
-        self.rem_viatura.grid(row = 2, column = 1, padx = 40, pady = 10, sticky = "ew")
-        self.gravar_catalogo.grid(row = 3, column = 0, padx = 40, pady = 10, sticky = "ew")
-        self.recarregar_catalogo.grid(row = 3, column = 1, padx = 40, pady = 10, sticky = "ew")
+        self.listar_viaturas.grid(row = 1, column = 0, padx = 40, pady = 10)
+        self.pes_viaturas.grid(row = 1, column = 1, padx = 40, pady = 10)
+        self.add_viatura.grid(row = 2, column = 0, padx = 40, pady = 10)
+        self.rem_viatura.grid(row = 2, column = 1, padx = 40, pady = 10)
+        self.gravar_catalogo.grid(row = 3, column = 0, padx = 40, pady = 10)
+        self.recarregar_catalogo.grid(row = 3, column = 1, padx = 40, pady = 10)
         
     def result_layout(self, font):
         self.result_layout_canvas = tk.Canvas(self)
-        self.result_layout_canvas.grid(row = 4, column = 0, columnspan = 2, padx = 0, pady = (80, 10), sticky = "ew")
+        self.result_layout_canvas.grid(row = 4, column = 0, columnspan = 2, padx = 0, pady = (80, 10))
         
         self.marca_titulo = tk.Label(self.result_layout_canvas, text = "Marca", font = font)
         self.modelo_titulo = tk.Label(self.result_layout_canvas, text = "Modelo", font = font)
         self.data_titulo = tk.Label(self.result_layout_canvas, text = "Data", font = font)
         self.matricula_titulo = tk.Label(self.result_layout_canvas, text = "Matricula", font = font)
         
-        self.marca_titulo.grid(row = 0, column = 0, padx =  40, pady = 0, sticky = "ew")
-        self.modelo_titulo.grid(row = 0, column = 1, padx = 40, pady = 0, sticky = "ew")
-        self.data_titulo.grid(row = 0, column = 2, padx = 40, pady = 0, sticky = "ew")
-        self.matricula_titulo.grid(row = 0, column = 3, padx = 40, pady = 0, sticky = "ew")
-        
-        for col in range(4):
-            self.result_layout_canvas.grid_columnconfigure(col, weight=1)
-        
+        self.marca_titulo.grid(row = 0, column = 0, padx =  40, pady = 0)
+        self.modelo_titulo.grid(row = 0, column = 1, padx = 40, pady = 0)
+        self.data_titulo.grid(row = 0, column = 2, padx = 40, pady = 0)
+        self.matricula_titulo.grid(row = 0, column = 3, padx = 40, pady = 0)
+                                            
     def limpar_result_layout(self, font):
         self.limpar_canvas = tk.Canvas(self)
-        self.limpar_canvas.grid(row = 5, column = 0, columnspan = 2, padx = 200, pady = 10, sticky = "ew")
-        
+        self.limpar_canvas.grid(row = 5, column = 0, columnspan = 2, padx = 200, pady = 10)
+                
         self.limpar_res_lay = tk.Button(self.limpar_canvas, text="Limpar", width = 8, height = 1,
-                                        font = font, command=lambda: print("clean"))
+                                        font = font, command = self.clean_list)
         self.limpar_res_lay.grid(row = 0, column = 0, padx =  40, pady = 0)
         self.limpar_canvas.grid_columnconfigure(0, weight=1)
-          
+        
+    def clean_list(self):
+        for group in self.detail_labels:
+            for label in group:
+                label.destroy()
+        self.detail_labels.clear
+        try:
+            self.list_scrollbar.destroy()
+            self.result_data_canvas.destroy()
+        except:
+            pass
+
     def action_listar_viaturas(self):
-        x = 1      
-        for car in self.carros:
-            tk.Label(self.result_layout_canvas, text = f"{car.marca}").grid(row = x, column = 0, padx =  40, pady = 0, sticky = "ew")
-            tk.Label(self.result_layout_canvas, text = f"{car.modelo}").grid(row = x, column = 1, padx =  40, pady = 0, sticky = "ew")
-            tk.Label(self.result_layout_canvas, text = f"{car.data}").grid(row = x, column = 2, padx =  40, pady = 0, sticky = "ew")
-            tk.Label(self.result_layout_canvas, text = f"{car.matricula}").grid(row = x, column = 3, padx =  40, pady = 0, sticky = "ew")
+        self.clean_list()
+        self.listar_resultados(self.carros)
+          
+    def listar_resultados(self, carros: CatalogoCarros):        
+        self.result_data_canvas = tk.Canvas(self.result_layout_canvas)
+        self.result_data_canvas.grid(row = 1, column = 0, columnspan = 4, sticky = 'ew')
+        self.result_layout_frame = tk.Frame(self.result_data_canvas)
+        # self.result_layout_frame = ScrolledText(self.result_data_canvas)
+        self.result_data_canvas.create_window((0, 0), window=self.result_layout_frame, anchor='nw')
+         
+        self.detail_labels.clear
+        x = 0
+        for car in carros:
+            label_marca = tk.Label(self.result_layout_frame, text = f"{car.marca}")
+            label_modelo = tk.Label(self.result_layout_frame, text = f"{car.modelo}")
+            label_data = tk.Label(self.result_layout_frame, text = f"{car.data}")
+            label_matricula = tk.Label(self.result_layout_frame, text = f"{car.matricula}")
+            
+            label_marca.grid(row = x, column = 0, padx =  40, pady = 0)
+            label_modelo.grid(row = x, column = 1, padx =  40, pady = 0)
+            label_data.grid(row = x, column = 2, padx =  40, pady = 0)
+            label_matricula.grid(row = x, column = 3, padx =  40, pady = 0)
+            
+            self.detail_labels.append((label_marca, label_modelo, label_data, label_matricula))
             x += 1
-                  
+                        
+        self.list_scrollbar = Scrollbar(self, orient = tk.VERTICAL, command = self.result_data_canvas.yview)
+        self.list_scrollbar.grid(row = 4, column = 2, pady = (110, 0), sticky="ns")
+        self.result_data_canvas.configure(yscrollcommand = self.list_scrollbar.set)
+        self.result_data_canvas.update_idletasks()
+        self.result_data_canvas.config(scrollregion=self.result_data_canvas.bbox("all"))
+                                                    
+        def on_mousewheel(event):
+            self.result_data_canvas.yview_scroll(-1 * (event.delta // 120), "units") 
+            
+        self.result_data_canvas.bind_all("<MouseWheel>", on_mousewheel)
+                                                                      
     def change_font(self, font_type: str):
         self.title_label.config(font = (font_type, 16, "bold"))
         self.listar_viaturas.config(font = (font_type, 12, "bold"))
